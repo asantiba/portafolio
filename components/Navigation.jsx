a8f0e6013ddc29370fff1e45bf8f78ebd54b8f63
asantiba <alexis.santibanez.14@sansano.usm.cl>
2023-02-03 11:38:12 -0300

    actualizacion react headroom

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -14,43 +14,39 @@ const Navigation = () => {
 
   const onExited = () => setCollapseClasses('')
 
-  useEffect(() => {
-    let headroom = new Headroom(document.getElementById('navbar-main'))
-    // initialise
-    headroom.init()
-  })
-
   return (
     <>
       <header className='header-global'>
-        <Navbar className='navbar-main navbar-transparent navbar-light headroom' expand='lg' id='navbar-main'>
-          <Container>
-            <NavbarBrand href='/' className='mr-lg-5'>
-              <h2 className='text-white' id='nav-title'>
-                {greetings.name}
-              </h2>
-            </NavbarBrand>
-            <button className='navbar-toggler' aria-label='navbar_toggle' id='navbar_global'>
-              <span className='navbar-toggler-icon' />
-            </button>
-            <UncontrolledCollapse toggler='#navbar_global' navbar className={collapseClasses} onExiting={onExiting} onExited={onExited}>
-              <div className='navbar-collapse-header'>
-                <Row>
-                  <Col className='collapse-brand' xs='6'>
-                    <h3 className='text-black' id='nav-title'>
-                      {greetings.name}
-                    </h3>
-                  </Col>
-                  <Col className='collapse-close' xs='6'>
-                    <button className='navbar-toggler' id='navbar_global'>
-                      <span />
-                      <span />
-                    </button>
-                  </Col>
-                </Row>
-              </div>
-            </UncontrolledCollapse>
-          </Container>
+        <Navbar className='navbar-main navbar-transparent navbar-light' expand='lg' id='navbar-main'>
+          <Headroom>
+            <Container>
+              <NavbarBrand href='/' className='mr-lg-5'>
+                <h2 className='text-white' id='nav-title'>
+                  {greetings.name}
+                </h2>
+              </NavbarBrand>
+              <button className='navbar-toggler' aria-label='navbar_toggle' id='navbar_global'>
+                <span className='navbar-toggler-icon' />
+              </button>
+              <UncontrolledCollapse toggler='#navbar_global' navbar className={collapseClasses} onExiting={onExiting} onExited={onExited}>
+                <div className='navbar-collapse-header'>
+                  <Row>
+                    <Col className='collapse-brand' xs='6'>
+                      <h3 className='text-black' id='nav-title'>
+                        {greetings.name}
+                      </h3>
+                    </Col>
+                    <Col className='collapse-close' xs='6'>
+                      <button className='navbar-toggler' id='navbar_global'>
+                        <span />
+                        <span />
+                      </button>
+                    </Col>
+                  </Row>
+                </div>
+              </UncontrolledCollapse>
+            </Container>
+          </Headroom>
         </Navbar>
       </header>
     </>
